76ac1109de56ca9b5d1dbca94b7fe57921a302a6
anglo331 <angloyasser@gmail.com>
2023-07-13 23:08:55 +0300

    second file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Start point to
+ * print (with proper grammar, but the outcome is a piece of art,)
+ *
+ * with no return
+*/
+
+void main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+}
