99e06669db509480e430a6e1a7ac3c155c55a9aa
anglo331 <angloyasser@gmail.com>
2023-07-15 20:14:37 +0300

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ * Return: returns zero in success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%i", i);
+	}
+
+	return (0);
+}
