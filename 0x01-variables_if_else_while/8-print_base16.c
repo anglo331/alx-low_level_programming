701886027a46b1b0c354c555d42f51272f5addb5
anglo331 <angloyasser@gmail.com>
2023-07-15 22:12:53 +0300

    test

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - lllllooalaadfaf
+ * Return: 0llldfd fdfjj kds
+ */
+
+int main(void)
+{
+	int i;
+	char x[] = "0123456789abcdef";
+
+	for (i = 0; i <= 16; i++)
+	{
+		putchar(x[i]);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
