570c49f818073e52621ca38944d2af5e83faa5f1
anglo331 <angloyasser@gmail.com>
2023-07-27 15:03:00 +0300

    hot fix

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 char *_strncat(char *dist, char *src, int n);
 char *_strcat(char *dist, char *src);
 char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
 
 #endif
