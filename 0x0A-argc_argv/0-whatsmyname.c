950d0eaeb1efd8fbca8b0ce36467425471ca299a
anglo331 <angloyasser@gmail.com>
2023-08-09 14:53:05 +0300

    hot fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	int i;
+	char *temp[] = argv[0];
+
+	for (i = 0 ; letter!= '\0'; i++)
+	{
+		_putchar(temp[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
