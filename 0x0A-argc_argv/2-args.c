48bdb108e9266a20424dcb22ccd5d2d6819da53e
anglo331 <angloyasser@gmail.com>
2023-08-10 01:23:09 +0300

    hot fix

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - to print the passed argument
+ * @argc: the number of argument
+ * @argv: array of arguments
+ *
+ * Return: 0 on succses
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argc[i])
+	}
+	return (0);
+}
