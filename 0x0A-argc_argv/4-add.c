771d5ea8faa4575f3d9fa658a0a8cb465376bb9d
anglo331 <angloyasser@gmail.com>
2023-08-10 02:04:09 +0300

    hot fix

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - to add args numbers
+ * @argc: n of args
+ * @argv: array of args
+ * Return: 0 if succsess
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		char y = argv[i];
+
+		if (y <= 'z' && y >= 'a')
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(y);
+	}
+	printf("%d\n", sum);
+	return 0;
+}
