070bba5132fcef66806a27ca7f5b8aff902c2bfe
anglo331 <angloyasser@gmail.com>
2023-08-14 14:17:27 +0300

    hot fix

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog object
+ *
+ * @name: the name of the doge
+ * @age: dog age in years
+ * @owner: the name of the owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
